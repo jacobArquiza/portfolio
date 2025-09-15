53222293f9b2c198707b4a7f92b04debe2126eee
Jacob Arquiza <jacob@Jacobs-MacBook-Air.local>
2025-09-15 05:12:10 -0700

    more changess

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,11 +1,11 @@
 var slideInFromLeftTargets = document.querySelectorAll(".slide-in-from-left-target");
 var slideInFromRightTargets = document.querySelectorAll(".project-item");
 var slideInFromTopTargets = document.querySelectorAll(".slide-in-from-top-target");
+var popTargets = document.querySelectorAll(".pop-target");
 
-function runAnimation(targets, animationName){
+function runAnimation(targets, animationName, interval){
     let startTime = null;
     let currentItem = 0;
-    const interval = 100; 
 
     function animate(timestamp) {
     if (!startTime) startTime = timestamp;
@@ -21,12 +21,24 @@ function runAnimation(targets, animationName){
                 if(item instanceof HTMLDivElement){
                     item.addEventListener("animationend", function(){
                         let input = this.querySelector("input");
-                        console.log(input);
                         input.checked = !input.checked;
                     })
                 }
             }
-
+            else if(animationName == "slide-in-from-right"){
+                if(item instanceof HTMLDivElement){
+                    item.addEventListener("animationend", function(){
+                        this.querySelectorAll(".pop-target").forEach(function(element){
+                            let randomNumber = Math.floor(Math.random() * 10) * 50 + 1;
+                            
+                            setTimeout(() => {
+                                element.classList.remove("hidden");
+                                element.classList.add("pop");
+                            }, randomNumber);
+                        });
+                    })
+                }
+            }
         currentItem++;
         }
     }
@@ -37,9 +49,56 @@ function runAnimation(targets, animationName){
     }
 
     requestAnimationFrame(animate);
+
 }
 
 
-runAnimation(slideInFromLeftTargets, "slide-in-from-left");
-runAnimation(slideInFromRightTargets, "slide-in-from-right");
-runAnimation(slideInFromTopTargets, "slide-in-from-top");
+runAnimation(slideInFromLeftTargets, "slide-in-from-left", 100);
+runAnimation(slideInFromRightTargets, "slide-in-from-right", 200);
+runAnimation(slideInFromTopTargets, "slide-in-from-top", 100);
+
+const byTechnologyButton = document.querySelector("#by-technology").querySelector("span");
+const byYearButton= document.querySelector("#by-year").querySelector("span");
+
+
+
+function addSortEventListeners(button){
+    button.addEventListener('mouseover', function(){
+        this.querySelectorAll("h2").forEach((h2)=>{
+            h2.classList.add("text-gray-500");
+        });
+    });
+    button.addEventListener('mouseout', function(){
+        this.querySelectorAll("h2").forEach((h2)=>{
+            if(h2.classList.contains("text-gray-500")){
+                h2.classList.remove("text-gray-500");
+            }
+        });
+    });
+    button.addEventListener('click', function(){
+        document.querySelectorAll(".card-body.webapp-project").forEach((project)=>{
+            if(project.classList.contains("slide-out")){
+                project.classList.remove("slide-out");
+            }
+            else{
+                project.classList.add("slide-out");
+            }
+        });
+    });
+}
+
+addSortEventListeners(byTechnologyButton);
+addSortEventListeners(byYearButton);
+
+byYearButton.addEventListener('mouseover', function(){
+    this.querySelectorAll("h2").forEach((h2)=>{
+        h2.classList.add("text-gray-500");
+    });
+});
+byYearButton.addEventListener('mouseout', function(){
+    this.querySelectorAll("h2").forEach((h2)=>{
+        if(h2.classList.contains("text-gray-500")){
+            h2.classList.remove("text-gray-500");
+        }
+    });
+});
